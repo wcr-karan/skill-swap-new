80ecffa43817916b4b2c37ebca21cf4218081476
Karan Thakur <ktthakur204@gmail.com>
2026-03-27 15:23:04 +0530

    feat(utils): add slugify helper

--- frontend/src/utils/stringUtils.js
+++ frontend/src/utils/stringUtils.js
@@ -1,2 +1,3 @@
 export const capitalize = (str) => str ? str.charAt(0).toUpperCase() + str.slice(1) : '';
 export const truncate = (str, len) => str && str.length > len ? str.substring(0, len) + '...' : str;
+export const slugify = (text) => text.toString().toLowerCase().replace(/\s+/g, '-').replace(/[^\w\-]+/g, '').replace(/\-\-+/g, '-').replace(/^-+/, '').replace(/-+$/, '');
