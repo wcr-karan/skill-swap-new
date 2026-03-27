15697b39692100dcb88dfff6e1cd4ae878f41f8e
Karan Thakur <ktthakur204@gmail.com>
2026-03-27 14:50:04 +0530

    feat(utils): add truncate function

--- frontend/src/utils/stringUtils.js
+++ frontend/src/utils/stringUtils.js
@@ -1,1 +1,2 @@
 export const capitalize = (str) => str ? str.charAt(0).toUpperCase() + str.slice(1) : '';
+export const truncate = (str, len) => str && str.length > len ? str.substring(0, len) + '...' : str;
