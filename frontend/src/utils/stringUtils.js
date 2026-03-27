523c1f0db405d3950003a5179fc466909e9dcdb2
Karan Thakur <ktthakur204@gmail.com>
2026-03-27 14:16:43 +0530

    refactor(utils): add jsdoc for capitalize

--- frontend/src/utils/stringUtils.js
+++ frontend/src/utils/stringUtils.js
@@ -1,3 +1,4 @@
 export const capitalize = (str) => str ? str.charAt(0).toUpperCase() + str.slice(1) : '';
 export const truncate = (str, len) => str && str.length > len ? str.substring(0, len) + '...' : str;
 export const slugify = (text) => text.toString().toLowerCase().replace(/\s+/g, '-').replace(/[^\w\-]+/g, '').replace(/\-\-+/g, '-').replace(/^-+/, '').replace(/-+$/, '');
+/** Capitalizes the first letter of a string */
