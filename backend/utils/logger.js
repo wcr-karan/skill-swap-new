0a0ae7dfd3ef7f0b189332160ded529de21e8fde
Karan Thakur <ktthakur204@gmail.com>
2026-03-29 15:42:46 +0530

    feat(logger): add logWarn function

--- backend/utils/logger.js
+++ backend/utils/logger.js
@@ -2,3 +2,5 @@ const logInfo = (msg) => console.log(`[INFO]: ${msg}`);
 module.exports = { logInfo };
 const logError = (msg) => console.error(`[ERROR]: ${msg}`);
 module.exports.logError = logError;
+const logWarn = (msg) => console.warn(`[WARN]: ${msg}`);
+module.exports.logWarn = logWarn;
