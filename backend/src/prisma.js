6888b36bc98ae47ad3e4bb4c8297de6206f648d3
Karan Thakur <ktthakur204@gmail.com>
2026-03-30 18:53:47 +0530

    Revert adapter bug

--- backend/src/prisma.js
+++ backend/src/prisma.js
@@ -1,17 +1,11 @@
 const { PrismaClient } = require("@prisma/client");
 const path = require("path");
-const Database = require("better-sqlite3");
 const { PrismaBetterSqlite3 } = require("@prisma/adapter-better-sqlite3");
 
-const dbPath = path.join(__dirname, "../dev.db");
+const adapter = new PrismaBetterSqlite3({
+  url: `file:${path.join(__dirname, "../dev.db")}`,
+});
 
-// Fallback for Render if ENV is missing
-if (!process.env.DATABASE_URL) {
-  process.env.DATABASE_URL = `file:${dbPath}`;
-}
-
-const sqlite = new Database(dbPath);
-const adapter = new PrismaBetterSqlite3(sqlite);
 const prisma = new PrismaClient({ adapter });
 
 module.exports = prisma;
