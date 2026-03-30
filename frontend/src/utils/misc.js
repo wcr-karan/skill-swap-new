89b714094af4400fb57aeae8f6a0d954206d5f70
Karan Thakur <ktthakur204@gmail.com>
2026-03-30 20:31:16 +0530

    chore: add helper function snippet 98

--- frontend/src/utils/misc.js
+++ frontend/src/utils/misc.js
@@ -3,3 +3,4 @@ export const snippet270 = () => 270;
 export const snippet747 = () => 747;
 export const snippet345 = () => 345;
 export const snippet458 = () => 458;
+export const snippet98 = () => 98;
