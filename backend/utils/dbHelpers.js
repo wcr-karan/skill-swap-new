5734b87d7f3036606c0f1770fe40b46c3ef677fe
Karan Thakur <ktthakur204@gmail.com>
2026-03-30 13:17:39 +0530

    feat(db): add pagination helper

--- backend/utils/dbHelpers.js
+++ backend/utils/dbHelpers.js
@@ -1,2 +1,4 @@
 const parseDbResult = (res) => res;
 module.exports = { parseDbResult };
+const getPagination = (page, size) => ({ limit: size, offset: page * size });
+module.exports.getPagination = getPagination;
